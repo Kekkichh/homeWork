ae0d1f0853e9daad46c4f9fb8f9da6c717db546e
Kekkichh <soloxin.kola2002@yandex.ru>
2024-08-12 14:03:21 +0300

    Add files via upload

---
+++ Peremen.py
@@ -0,0 +1,8 @@
+#Практическое задание по работе в Pycharm - "Переменные".
+
+name = 'Python'
+tasks = '12'
+hours = '1.5'
+average_time = (float(hours) / int(tasks))
+
+print('Курс:', name, 'всего задач:', tasks ,'затрачено часов:', hours, 'среднее время выполнения:', average_time)
