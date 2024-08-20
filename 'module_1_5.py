7a4c68f71cfdb76f209bff77ce1a560aacef2dd8
Kekkichh <soloxin.kola2002@yandex.ru>
2024-08-20 14:55:30 +0300

    Add files via upload

---
+++ 'module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, True, 'Zenfa'
+print(immutable_var)
+# immutable_var [1] = 12 не изменяемый
+# print(immutable_var)
+mutable_list = [1, 2, True, 'Zenfa']
+mutable_list [1] = 15
+mutable_list [3] = 'Мир'
+print(mutable_list)
